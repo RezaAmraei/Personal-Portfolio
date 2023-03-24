8ece5d8e3b68b6a5016302bfaa0dbe110bd41ebc
Reza Amraei <amraeireza@gmail.com>
2023-03-24 13:30:05 -0700

    feat: idk

---
+++ src/Components/SpinningName.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SpinningName = () => {
+  return <div>SpinningName</div>;
+};
+
+export default SpinningName;
---
+++ src/Views/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../CSS/HomePage.css";
+const HomePage = () => {
+  console.log(2 + 2);
+  return (
+    <div id="homePageBody">
+      <img src={`../Img/solarSystem.jpg`} />
+    </div>
+  );
+};
+
+export default HomePage;
