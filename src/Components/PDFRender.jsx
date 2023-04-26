fea966aa32d01a435d65a133c51f1d2713610826
Reza Amraei <amraeireza@gmail.com>
2023-04-26 14:11:39 -0700

    feat: added new div for mobile pdf view

--- src/Components/PDFRender.jsx
+++ src/Components/PDFRender.jsx
@@ -8,22 +8,49 @@ import "../CSS/pdf-render.css";
 
 const PDFRender = () => {
   return (
-    <div className="pdf-container" data-aos="flip-up" data-aos-delay="300">
-      <a href={Resume} download>
-        <Button shape="round" icon={<DownloadOutlined />} size="large">
-          Download
-        </Button>
-      </a>
-      <Document file={Resume}>
-        <Page
-          pageNumber={1}
-          renderTextLayer={false}
-          renderAnnotationLayer={false}
-          loading={"Loading Resume..."}
-          scale={1.6}
-        />
-      </Document>
-    </div>
+    <>
+      <div
+        className="pdf-container pdf-desktop"
+        data-aos="flip-up"
+        data-aos-delay="300"
+      >
+        <a href={Resume} download>
+          <Button shape="round" icon={<DownloadOutlined />} size="large">
+            Download
+          </Button>
+        </a>
+        <Document file={Resume}>
+          <Page
+            pageNumber={1}
+            renderTextLayer={false}
+            renderAnnotationLayer={false}
+            loading={"Loading Resume..."}
+            scale={1.6}
+          />
+        </Document>
+      </div>
+
+      <div
+        className="pdf-container pdf-mobile"
+        data-aos="flip-up"
+        data-aos-delay="300"
+      >
+        <a href={Resume} download className="mobile-download-button">
+          <Button shape="round" icon={<DownloadOutlined />} size="large">
+            Download
+          </Button>
+        </a>
+        <Document file={Resume}>
+          <Page
+            pageNumber={1}
+            renderTextLayer={false}
+            renderAnnotationLayer={false}
+            loading={"Loading Resume..."}
+            scale={0.7}
+          />
+        </Document>
+      </div>
+    </>
   );
 };
 
